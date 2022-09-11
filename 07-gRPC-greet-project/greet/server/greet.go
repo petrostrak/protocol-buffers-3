5bb3fca1f3a0b79797e11c3233e78ab585d0d6d2
petrostrak <pit.trak@gmail.com>
2022-09-11 18:39:24 +0300

    Refactor Server Streaming impl

--- 07-gRPC-greet-project/greet/server/greet.go
+++ 07-gRPC-greet-project/greet/server/greet.go
@@ -3,12 +3,29 @@ package main
 import (
 	pb "07-grpc-greet-project/greet/proto"
 	"context"
+	"fmt"
 	"log"
 )
 
+// Unary API Server Implementation
 func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
 	log.Printf("Greet function was invoked with %v\n", in)
 	return &pb.GreetResponse{
 		Result: "Hello " + in.FirstName,
 	}, nil
+}
+
+// Server Streaming Implementation
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+	log.Printf("GreetManyTimes() invoked with: %v\n", in)
+
+	for i := 0; i < 10; i++ {
+		res := fmt.Sprintf("Hello %s, number %d", in.FirstName, i)
+
+		stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+	}
+
+	return nil
 }

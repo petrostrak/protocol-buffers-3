4e6a8a2a11c8e054fd7784cd034c10c127512d02
petrostrak <pit.trak@gmail.com>
2022-09-11 17:57:17 +0300

    Server Streaming API Server Implementation

---
+++ 07-gRPC-greet-project/greet/server/greet_many_times.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	pb "07-grpc-greet-project/greet/proto"
+	"fmt"
+	"log"
+)
+
+func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
+	log.Printf("GreetManyTimes() invoked with: %v\n", in)
+
+	for i := 0; i < 10; i++ {
+		res := fmt.Sprintf("Hello %s, number %d", in.FirstName, i)
+
+		stream.Send(&pb.GreetResponse{
+			Result: res,
+		})
+	}
+
+	return nil
+}

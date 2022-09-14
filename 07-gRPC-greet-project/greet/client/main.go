bca909991c4ce0ed47975e09b07fdd112cc3e719
petrostrak <pit.trak@gmail.com>
2022-09-14 15:29:21 +0300

    Modify client for ssl security connections

--- 07-gRPC-greet-project/greet/client/main.go
+++ 07-gRPC-greet-project/greet/client/main.go
@@ -7,7 +7,7 @@ import (
 	pb "07-grpc-greet-project/greet/proto"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
@@ -15,7 +15,22 @@ var (
 )
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+		if err != nil {
+			log.Printf("error while loading CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	// conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(addr, opts...)
+
 	if err != nil {
 		log.Printf("failed to connect: %v\n", err)
 	}

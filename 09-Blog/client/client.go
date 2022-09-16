78f31c98621038c76e1402e4ee200b5acc23183c
petrostrak <pit.trak@gmail.com>
2022-09-16 13:04:33 +0300

    Implement client-side readblog function

--- 09-Blog/client/client.go
+++ 09-Blog/client/client.go
@@ -10,9 +10,9 @@ func createBlog(c pb.BlogServiceClient) string {
 	log.Println("createBlog() invoked!")
 
 	blog := &pb.Blog{
-		AuthorId: "Petros",
-		Title:    "My First Blog",
-		Content:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		AuthorId: "Eirini",
+		Title:    "Vietnam Blog",
+		Content:  "Vietnam is a Southeast Asian country known for its beaches, rivers, Buddhist pagodas and bustling cities. Hanoi, the capital, pays homage to the nation’s iconic Communist-era leader, Ho Chi Minh, via a huge marble mausoleum. Ho Chi Minh City (formerly Saigon) has French colonial landmarks, plus Vietnamese War history museums and the Củ Chi tunnels, used by Viet Cong soldiers.",
 	}
 
 	res, err := c.CreateBlog(context.Background(), blog)
@@ -23,4 +23,18 @@ func createBlog(c pb.BlogServiceClient) string {
 	log.Printf("Blog has been created with a new id of %s.\n", res.Id)
 
 	return res.Id
+}
+
+func readBlog(c pb.BlogServiceClient, id string) *pb.Blog {
+	log.Println("readBlog() invoked!")
+
+	req := &pb.BlogId{Id: id}
+	res, err := c.ReadBlog(context.Background(), req)
+	if err != nil {
+		log.Printf("err while reading err %v\n", err)
+	}
+
+	log.Printf("Blog was read: %v\n", res)
+
+	return res
 }
--- 09-Blog/client/main.go
+++ 09-Blog/client/main.go
@@ -21,5 +21,6 @@ func main() {
 
 	c := pb.NewBlogServiceClient(conn)
 
-	createBlog(c)
+	id := createBlog(c)
+	readBlog(c, id)
 }

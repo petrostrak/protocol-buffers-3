ec3e8c6d894aced33f946dd2093d836626aa6e91
petrostrak <pit.trak@gmail.com>
2022-10-03 14:04:18 +0300

    Implement UpdateBlog sever

--- 09-Blog/server/server.go
+++ 09-Blog/server/server.go
@@ -11,6 +11,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *Server) CreateBlog(ctx context.Context, in *pb.Blog) (*pb.BlogId, error) {
@@ -68,7 +69,44 @@ func (s *Server) ReadBlog(ctx context.Context, in *pb.BlogId) (*pb.Blog, error) 
 	return documentToBlog(data), nil
 }
 
-func (s *Server) UpdateBlog(ctx context.Context, in *pb.Blog) (*empty.Empty, error) { return nil, nil }
+func (s *Server) UpdateBlog(ctx context.Context, in *pb.Blog) (*empty.Empty, error) {
+	log.Printf("UpdateBlog() invoked with %v\n", in)
+
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("error parse ID: %v\n", err),
+		)
+	}
+
+	data := &BlogItem{
+		AuthorId: in.AuthorId,
+		Title:    in.Title,
+		Content:  in.Content,
+	}
+
+	res, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": oid},
+		bson.M{"$set": data},
+	)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Could not update",
+		)
+	}
+
+	if res.MatchedCount == 0 {
+		return nil, status.Errorf(
+			codes.NotFound,
+			"Cannot find blog with Id",
+		)
+	}
+
+	return &emptypb.Empty{}, nil
+}
 
 func (s *Server) DeleteBlog(ctx context.Context, in *pb.BlogId) (*empty.Empty, error) {
 	return nil, nil

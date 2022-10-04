96279938a5aa0037af7c6b80d55670025de490a8
petrostrak <pit.trak@gmail.com>
2022-10-04 15:37:55 +0300

    Implement DeleteBlog server

--- 09-Blog/server/server.go
+++ 09-Blog/server/server.go
@@ -109,7 +109,32 @@ func (s *Server) UpdateBlog(ctx context.Context, in *pb.Blog) (*empty.Empty, err
 }
 
 func (s *Server) DeleteBlog(ctx context.Context, in *pb.BlogId) (*empty.Empty, error) {
-	return nil, nil
+	log.Printf("DeleteBlog() invoked with %v\n", in)
+
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("error parse ID: %v\n", err),
+		)
+	}
+
+	res, err := collection.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Could not delete object in mongoDB: %v\n", err),
+		)
+	}
+
+	if res.DeletedCount == 0 {
+		return nil, status.Errorf(
+			codes.NotFound,
+			"Blog was not found",
+		)
+	}
+
+	return &emptypb.Empty{}, nil
 }
 
 func (s *Server) ListBlogs(in *empty.Empty, stream pb.BlogService_ListBlogsServer) error {
